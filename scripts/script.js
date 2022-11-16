fb29dc20a1e59101f1a33467a5d45ec82c19cf6b
T-Bax <69568581+T-Bax@users.noreply.github.com>
2022-11-16 22:16:38 +1100

    Work on Epoch Converter

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,67 @@
-console.log(new Date(2022,05,14,13,55));
+const notifications = [
+    {
+        noun: "Mark Webber",
+        verb: "reacted to your recent post",
+        subject: "My first tournament day!",
+        read: false,
+        timestamp: new Date(2022,11,13,15,20)
+    }
+];
+
+const epochToMinutes = (epoch) => {
+    const yrs = Math.floor(epoch/60000);
+    return yrs;
+}
+const epochToMonths = (epoch) => {
+    const yrs = Math.floor(epoch/2629743000);
+    return yrs;
+}
+const epochToHours = (epoch) => {
+    const yrs = Math.floor(epoch/3600000);
+    return yrs;
+}
+const epochToDays = (epoch) => {
+    const yrs = Math.floor(epoch/86400000);
+    return yrs;
+}
+const epochToWeeks = (epoch) => {
+    const yrs = Math.floor(epoch/604800000);
+    return yrs;
+}
+const epochToYears = (epoch) => {
+    const yrs = Math.floor(epoch/31556926000);
+    return yrs;
+}
+
+const convertFromEpoch = (epoch) => {
+    switch(true){
+        case epoch > 31556926000: 
+            return epochToYears(epoch)
+        case epoch > 2629743000: 
+            return epochToMonths(epoch)
+        case epoch > 604800000: 
+            return epochToWeeks(epoch)
+        case epoch > 86400000: 
+            return epochToDays(epoch)
+        case epoch > 3600000: 
+            return epochToHours(epoch)
+        default: 
+            return epochToMinutes(epoch)
+        
+    }
+
+}
+
+
+
+
+const d = new Date("2022-02-15").getTime();
+const e = new Date("2022-03-26").getTime();
+
+let a = (Date.now());
+let b = notifications[0].timestamp.getTime();
+
+console.log(d,e)
+let dif = e - d
+console.log(dif);
+console.log(convertFromEpoch(dif))
